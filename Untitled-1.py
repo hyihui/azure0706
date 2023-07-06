7ab1fddd001e9854e791f3ec80ac1b04f0e97d0c
hyihui <138743611+hyihui@users.noreply.github.com>
2023-07-06 14:16:31 +0800

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,36 @@
-print("test")
+import openai
+from flask import Flask, request, jsonify
+
+# Set up OpenAI API credentials
+openai.api_key = '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'
+
+# Create Flask web application
+app = Flask(__name__)
+
+# Define a route for the question-answering endpoint
+@app.route('/ask', methods=['POST'])
+def ask_question():
+    # Get the question from the request
+    question = request.json['question']
+
+    # Generate the answer using GPT-3.5
+    response = openai.Completion.create(
+        engine='text-davinci-003',  # Specify GPT version (GPT-3.5)
+        prompt=question,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+
+    # Extract the generated answer from the API response
+    answer = response.choices[0].text.strip()
+
+    # Return the answer as a JSON response
+    return jsonify({'answer': answer})
+
+# Run the Flask application
+if __name__ == '__main__':
+    app.run()
+
+
